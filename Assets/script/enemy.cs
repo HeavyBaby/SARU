509861f880a1eee307fc60a3b4daac6109c5e30e
DA\nwuser <puzz_ryu_777@docomo.ne.jp>
2017-03-13 15:51:43 +0900

    猿が移動する

---
+++ Assets/script/enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+    private Vector2 speed = new Vector2(0.05f, 0.05f);
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector2 enemypos = transform.position;
+        enemypos.x = enemypos.x - speed.x;
+        transform.position = enemypos;
+    }
+}
---
+++ Assets/script/spon.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spon : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    // Use this for initialization
+    int cnt = 0;
+    void Start()
+    {
+        StartCoroutine(Proc());
+    }
+    IEnumerator Proc()
+    {
+
+        while (true)
+        {
+            float x = 5;
+            float y = 5;
+            
+            Vector2 pos = new Vector2(x, y);
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            CreateEnemy(pos);
+            cnt++;
+            yield return new WaitForSeconds(60f);
+            if (cnt < 10) break;
+            
+        }
+    }
+
+    void CreateEnemy(Vector2 position)
+    {
+        Instantiate(enemyPrefab, position, transform.rotation);
+    }
+}
+
