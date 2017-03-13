831b9634e80409c4ca9211f6325f108ffa8a537d
DA\nwuser <kou.karu.ramu10@icloud.com>
2017-03-13 14:49:30 +0900

    バナナの平行移動

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity
+		= new Vector2 (5.0f, 0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
+
